495c50d57664713cdaa5fcfaac972ab2066850b9
jbdel <jbdjbd001@gmail.com>
2018-05-02 18:05:46 +0200

    modif

--- audio_model/mp4_to_wav.py
+++ audio_model/mp4_to_wav.py
@@ -3,30 +3,42 @@ import fnmatch
 import subprocess
 import sys
 import csv
+import argparse
 
-if not os.path.exists("data"):
-    os.makedirs("data")
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--video_path', type=str, default="/media/jb/DATA/OMGEmotionChallenge/train")
+parser.add_argument('--out_dir', type=str, default="./wav/")
+args = parser.parse_args()
+
+
+data_dir = "./data/"
+
+if not os.path.exists(args.out_dir):
+    os.makedirs(args.out_dir)
+
 
 matches = []
-for root, dirnames, filenames in os.walk('/media/jb/DATA/OMGEmotionChallenge/train'):
+for root, dirnames, filenames in os.walk(args.video_path):
     for filename in fnmatch.filter(filenames, '*.mp4'):
         matches.append(os.path.join(root, filename))
 
 
 filter = [x for x in matches if "temp" not in x]
 print(len(filter),"files found")
-for mode in ["Train","Validation"]:
+for mode in ["Train","Validation", "Test"]:
 
-    ABS = "/media/jb/DATA/OMGEmotionChallenge/"
 
-    with open(ABS+"omg_{}Videos.csv".format(mode), 'r') as csvfile:
+    with open(data_dir+"omg_{}Videos.csv".format(mode), 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             found = False
             for x in filter:
                 if (row['video'] in x) and (row['utterance'] in x):
                     blocks = x.split("/")
-                    outfile = os.path.join("data","{}#{}".format(blocks[-2], blocks[-1]).replace("mp4", "wav"))
+                    outfile = os.path.join(args.out_dir,"{}#{}".format(blocks[-2], blocks[-1]).replace("mp4", "wav"))
                     if os.path.exists(outfile) and os.path.getsize(outfile) > 0:
                         print(outfile, "already exists")
                     else:
--- audio_model/sample_features.py
+++ audio_model/sample_features.py
@@ -4,13 +4,17 @@ import numpy as np
 import os
 import csv
 
+data_path = "data/"
 
+x = np.load(os.path.join(data_path,"IS13_ComParE_train.npy")).astype(np.float)
+y = np.load(os.path.join(data_path,"IS13_ComParE_valid.npy")).astype(np.float)
+z = np.load(os.path.join(data_path,"test.npy")).astype(np.float)
 
-x = np.load("IS13_ComParE_train.npy").astype(np.float)
-y = np.load("IS13_ComParE_valid.npy").astype(np.float)
+print("Original feature size : ", x.shape[1])
 
+#we load train target
 utterances_x = []
-with open(os.path.join("omg_TrainVideos.csv"), 'r') as csvfile:
+with open(os.path.join(os.path.join(data_path,"omg_TrainVideos.csv")), 'r') as csvfile:
     reader = csv.DictReader(csvfile)
     for i, row in enumerate(reader):
         utterances_x.append([row['arousal'], row['valence']])
@@ -19,19 +23,22 @@ with open(os.path.join("omg_TrainVideos.csv"), 'r') as csvfile:
 
 u = np.array(utterances_x).astype(np.float)
 
-
+#we select best features according to both arousal and valence
 s1 = SelectKBest(f_regression, k=80).fit(x, u[:,0])
 s2 = SelectKBest(f_regression, k=80).fit(x, u[:,1])
 r1 = s1.get_support(True)
 r2 = s2.get_support(True)
 
 
+#we merge best features
 l = len(set(np.concatenate([r1,r2])))
 
 print("feature size",l)
 
+#for each dataset, we get the chosen features
 train = np.zeros((x.shape[0],l))
 valid = np.zeros((y.shape[0],l))
+test = np.zeros((z.shape[0],l))
 
 for i in range(x.shape[0]):
     for j,indice in enumerate(set(np.concatenate([r1,r2]))):
@@ -41,6 +48,12 @@ for i in range(y.shape[0]):
     for j,indice in enumerate(set(np.concatenate([r1,r2]))):
         valid[i][j] = y[i][indice]
 
+for i in range(z.shape[0]):
+    for j,indice in enumerate(set(np.concatenate([r1,r2]))):
+        test[i][j] = z[i][indice]
 
-np.save("audio_train", train)
-np.save("audio_validation", valid)
+
+
+np.save(os.path.join(data_path,"audio_train"), train)
+np.save(os.path.join(data_path,"audio_validation"), valid)
+np.save(os.path.join(data_path,"audio_test"), test)
